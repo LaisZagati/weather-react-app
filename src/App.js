de8d2563d730d65a8c398180d4b477012b2c490e
LaisZagati <laisviana97@gmail.com>
2025-10-02 14:14:54 -0400

    Uplaoded to github and deployed on Netlify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Lais Zagati and is
+        <a
+          href="https://github.com/LaisZagati/weather-react-app"
+          target="blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
