3ee72ccb08abcef838b730076b43003774973e7f
LaisZagati <laisviana97@gmail.com>
2025-10-03 19:37:27 -0400

    React Weather Project / Forecast / API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,26 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "ab8e7ef210556986d1c9a75d6007b825";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Mon</div>
-                  <WeatherIcon code="01d" size={36} />
+          <WeatherIcon code="01d" size={36} />
           <div className="WeatherForecast-temperature">
             <span className="WeatherForecast-temperature-max">19°</span>
             <span className="WeatherForecast-temperature-min">10°</span>
