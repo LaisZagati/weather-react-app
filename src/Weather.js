335253bbd8354c1521611b8c7395315af16e1a38
LaisZagati <laisviana97@gmail.com>
2025-10-03 10:39:31 -0400

    Css features added to weather components

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,14 +29,20 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
-            alt="Mostly Cloud"
-          />
-          6°C
-        </div>
+          {/* <div className="clearfix"> */}
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+              alt="Mostly Cloud"
+            //   className="float-left"
+            />
+            {/* <div className="float-left"> */}
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          {/* </div>
+        </div> */}
         <div className="col-6">
           <ul>
             <li>Precipitation: 15%</li>
